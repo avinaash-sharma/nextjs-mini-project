3a04c21630aabb9fb9389c18c8b013abecc329f4
Avinash Sharma <avi.rohit13@hotmail.com>
2025-03-29 02:44:57 +0530

    pushed search and filter

--- constants/routes.ts
+++ constants/routes.ts
@@ -3,6 +3,7 @@ export const ROUTES = {
   SIGN_UP: "/sign-up",
   SIGN_IN: "/sign-in",
   NOT_FOUND: "/404",
+  ASK_QUESTION: "/ask-question",
   PROFILE: (id: string) => `profile/${id}`,
   QUESTION: (id: string) => `question/${id}`,
   TAGS: (id: string) => `tags/${id}`,
