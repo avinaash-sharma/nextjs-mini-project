3b26a2a3369623d785c1d33e8f72220f311b352b
Avinash Sharma <avi.rohit13@hotmail.com>
2025-05-22 04:02:30 +0530

    alot is still left

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 
+import QuestionForm from "@/components/forms/QuestionForm";
+
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a public question</h1>
+      <div className="mt-9">
+        <QuestionForm />
+      </div>
+    </div>
+  );
 };
 
 export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -105,7 +105,8 @@ const config: Config = {
   	}
   },
 
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
+
 };
 
 export default config;
