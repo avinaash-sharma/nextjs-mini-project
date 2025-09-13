a2acb9a3f7e38b20f0d16a57232a746da9075a45
Avinash Sharma <avi.rohit13@hotmail.com>
2025-09-13 11:18:25 +0530

    create and read accounts done!

---
+++ app/api/accounts/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from "next/server";
+
+import Account from "@/database/account.model";
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { ForbiddenError, ValidationError } from "@/lib/http-errors";
+import dbConnect from "@/lib/mongoose";
+import { AccountSchema, UserSchema } from "@/lib/validations";
+import { APIErrorResponse } from "@/types/global";
+
+export async function GET() {
+  try {
+    await dbConnect();
+    const accounts = await Account.find();
+    return NextResponse.json(
+      { success: true, data: accounts },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    await dbConnect();
+    const body = await request.json();
+
+    const validatedData = AccountSchema.parse(body);
+
+    // to check if the data is comming fine from frontend.
+    const exisitingAccount = await Account.findOne({
+      provider: validatedData.provider,
+      providerAccountId: validatedData.providerAccountId,
+    });
+
+    if (exisitingAccount)
+      throw new ForbiddenError("An account with the same provider exists");
+
+    const newAccount = await Account.create(validatedData);
+    return NextResponse.json(
+      { success: true, data: newAccount },
+      { status: 201 }
+    );
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
